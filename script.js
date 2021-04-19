ab1fb0835e5f4b7362d8cf4facae5f0fdcd80475
Cfee <cfee89@gmail.com>
2021-04-19 08:32:43 -0400

    Implemented arrays of randoms nouns,verbs, things, generated random indices for each, interpolated into message and logged to console.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Test test test");
+
+const nouns = ['A tiger','Lisa', 'Craig','A bat'];
+const verbs = ['yelled at','ate', 'caught','tripped'];
+const things = ['bowl','guy','lamp','lamb'];
+
+const randoNoun = Math.floor(Math.random()*4);
+const randoVerb = Math.floor(Math.random()*4);
+const randoThing = Math.floor(Math.random()*4);
+
+const message = `${nouns[randoNoun]} ${verbs[randoVerb]} the ${things[randoThing]}.`;
+console.log(message);
